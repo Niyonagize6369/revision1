a9a38b39d2eba18484d943c942ab4ca3689dda30
Your Name <niyonagizerachel10@email.com>
2025-05-22 20:09:43 -0700

    revision pushed

--- Desktop/revision/index.js
+++ Desktop/revision/index.js
@@ -7,3 +7,13 @@ console.log(id);
 //cannot start with a number (1name)
 // can not contain a space or hyphen(-)
  
+// let firstname ="mosh";
+// let lastName = "hamdani";
+
+// either 
+
+// let firstName = "mosh", lastName = "hamdani";
+
+// let interestRate = 0.9;(it is possible to change the variable)
+// interestRate =5
+// console.log(interestRate): when i use const means that the variable will be constant
