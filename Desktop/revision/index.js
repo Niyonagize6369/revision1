7c91e9e6625081d4c6fe01fc74f524c39b7c169a
Your Name <niyonagizerachel10@email.com>
2025-05-22 20:00:03 -0700

    revision pushed

---
+++ Desktop/revision/index.js
@@ -0,0 +1,9 @@
+// console.log('Hello World')
+let id= 7;
+console.log(id);
+
+//cannot be a reserved keyword
+//should be meangfull
+//cannot start with a number (1name)
+// can not contain a space or hyphen(-)
+ 
