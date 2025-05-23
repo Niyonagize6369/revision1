ddf1e6af768ee484b8042f8b3351a74a4e95dc55
Your Name <niyonagizerachel10@email.com>
2025-05-22 20:43:24 -0700

    revision pushed

--- Desktop/revision/index.js
+++ Desktop/revision/index.js
@@ -7,8 +7,11 @@ console.log(id);
 //cannot start with a number (1name)
 // can not contain a space or hyphen(-)
  
-// let firstname ="mosh";
+// let firstname ="mosh"; string
 // let lastName = "hamdani";
+// let age=80; number
+// let isApproved = false boolean
+// let selectColor = null;
 
 // either 
 
@@ -17,3 +20,38 @@ console.log(id);
 // let interestRate = 0.9;(it is possible to change the variable)
 // interestRate =5
 // console.log(interestRate): when i use const means that the variable will be constant
+
+
+// how  we create Object
+
+let person = {
+    name:'mosh',
+    age:80
+
+};
+console.log(person);
+
+
+// dot notation 
+
+person.age= 67;
+console.log(person.age);
+
+// blacket notation
+
+person ['name'] = 'mary';
+console.log(person.name)
+
+
+// Array
+
+ let selectColor = ['black,yellow'];
+ selectColor[2]= 'green';
+ console.log(selectColor);
+ console.log(selectColor.length)
+
+//  functions 
+
+
+
+
